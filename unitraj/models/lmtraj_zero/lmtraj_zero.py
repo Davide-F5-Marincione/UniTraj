d3c575bb90aa7d24275d691988563a80a9ae632f
AlessioSam <alessiosampieri27@gmail.com>
2024-07-27 13:27:48 +0200

    started working

--- unitraj/models/lmtraj_zero/lmtraj_zero.py
+++ unitraj/models/lmtraj_zero/lmtraj_zero.py
@@ -7,6 +7,11 @@ from torch.distributions import MultivariateNormal, Laplace
 from unitraj.models.base_model.base_model import BaseModel
 import transformers
 import os
+from tqdm import tqdm
+import re
+
+max_timeout = 20
+temperature = 0.7
 
 # class AutoBotEgo(nn.Module):
 class LMTrajZero(BaseModel):
@@ -21,6 +26,9 @@ class LMTrajZero(BaseModel):
         self.config = config
         self.c = config['num_modes']
         self.model_id = config['model_id']
+        self.T = config['future_len']
+        self.past = config['past_len']
+        self.max_new_tokens = config['max_new_tokens']
 
         os.environ["TOKENIZERS_PARALLELISM"] = 'false'
 
@@ -30,24 +38,132 @@ class LMTrajZero(BaseModel):
             model_kwargs={"torch_dtype": torch.float16},
             device="cuda"
         )
-        
-        messages = [
-            {"role": "system", "content": "You are a helpful assistant that Extrapolate the coordinate sequence data."},
-            {"role": "user", "content": "Who are you?"},
-        ]
 
-        outputs = self.pipeline(
-            messages,
-            max_new_tokens=256,
-        )
-        print(outputs[0]["generated_text"][-1])
+        self.prompt_system = "You are a helpful assistant that Extrapolate the coordinate sequence data."
+        self.prompt_template_list = ["Forecast {3:d} times the next {1:d} (x, y) coordinates using the observed {0:d} (x, y) coordinate list.\nDirectly output the {3:d} different cases of the future {1:d} coordinate Python list without explanation.\nList must be in single line, without line split or numbering.\nDirectly output final lists with {1:d} x-y coordinates each without for loop or multiply operators.\n{2:s}",
+                        "Give me {3:d} more results other than the above methods.",
+                        "Give me {3:d} more results other than the above methods.",
+                        "Give me {3:d} more results other than the above methods."]
+        self.coord_template = "({0:.2f}, {1:.2f})"
+        self.scene_name_template = "scene_{:04d}"
+        
 
         self.criterion = Criterion(self.config)
 
     def forward(self, batch):
         inputs = batch['input_dict']
+
+        num_scenes = inputs['obj_trajs'].shape[0]
+
+        for scene_idx in (progressbar:=tqdm(range(num_scenes), desc='Chatbot started!')):
+
+            obs_traj = inputs['obj_trajs'][scene_idx,..., :2].cpu().numpy()
+            pred_traj = inputs['center_gt_trajs'][scene_idx,..., :2].cpu().numpy()
+            num_ped, obs_len, _ = obs_traj.shape
+            pred_len, _ = pred_traj.shape
+
+            llm_response_list = [["" for _ in range(len(self.prompt_template_list))] for _ in range(num_ped)]
+            llm_processed_list = [[] for _ in range(num_ped)]
+        
+            for ped_idx in range(num_ped):
+                messages = [{"role": "system", "content": self.prompt_system}]
+
+                for prompt_idx in range(len(self.prompt_template_list)):
+                    coord_str = '[' + ', '.join([self.coord_template.format(*obs_traj[ped_idx, i]) for i in range(obs_len)]) + ']'
+                    prompt = self.prompt_template_list[prompt_idx].format(obs_len, pred_len, coord_str, self.c)
+                    print(prompt)
+                    messages.append({"role": "user", "content": prompt})
+                    
+                    error_code = ''
+                    timeout = 0
+                    add_info = 0
+
+                    while timeout < max_timeout:
+                        # Prevent RateLimitError by waiting for 20 seconds
+                        progressbar.set_description('Ped_id {}/{} Prompt_no. {}/{} retry {}/{} {}'.format(ped_idx+1, num_ped, prompt_idx+1, len(self.prompt_template_list), timeout, max_timeout, error_code))
+                        
+                        # Set additional information and settings when it kept failing
+                        tmp = 1.0 if timeout >= max_timeout // 2 else temperature
+                        if prompt_idx == 0 and timeout == max_timeout // 4 and add_info < 1:
+                            messages[-1]['content'] += '\nProvide five hypothetical scenarios based on different extrapolation methods.'
+                            add_info = 1
+                        elif prompt_idx == 0 and timeout == (max_timeout // 4) * 2 and add_info < 2:
+                            messages[-1]['content'] += '\nYou can use methods like linear interpolation, polynomial fitting, moving average, and more.'
+                            add_info = 2
+                        
+                        # Run the Chatbot model
+                        try:
+                            response = self.pipeline(
+                                messages,
+                                max_new_tokens=self.max_new_tokens,
+                                temperature=tmp,
+                            )
+                            response = response[0]["generated_text"][-1]['content']
+                            # print('Coord_string--------------------\n', coord_str)
+                            # print('Response------------------------\n', response)
+                        except Exception as err:
+                            error_code = f"Unexpected {err=}, {type(err)=}"
+                            print(error_code)
+                            response = ''
+                            timeout += 1
+                            continue
+
+                        # filter out wrong answers
+                        if (not (abs(obs_traj[ped_idx, 0] - obs_traj[ped_idx, -1]).sum() < 0.3
+                            or abs(obs_traj[ped_idx, 0] - obs_traj[ped_idx, 2]).sum() < 0.2
+                            or abs(obs_traj[ped_idx, -3] - obs_traj[ped_idx, -1]).sum() < 0.2) and '[' + self.coord_template.format(*obs_traj[ped_idx, 0]) in response):
+                            if prompt_idx == 0:
+                                timeout += 1
+                                error_code = 'Obs coordinates included'
+                                continue
+                        elif ('[' + self.coord_template.format(*obs_traj[ped_idx, 0, ::-1]) in response
+                            or '(x4, y4)]' in response
+                            or ')]' not in response):
+                            timeout += 1
+                            error_code = 'Invalid response shape'
+                            continue
+                        elif len(response) == 0:
+                            timeout += 1
+                            error_code = 'Empty response'
+                            continue
+                        
+                        # Convert to list, check validity
+                        try:
+                            response_cleanup = re.sub('[^0-9()\[\],.\-\n]', '', response.replace(':', '\n')).replace('(', '[').replace(')', ']')
+                            response_cleanup = [eval(line) for line in response_cleanup.split('\n') if len(line) > 20 and line.startswith('[[') and line.endswith(']]')]
+                        except:
+                            timeout += 1
+                            error_code = 'Response to list failed'
+                            continue
+                        
+                        # Remove repeated obs sequence or truncate the response
+                        if len(response_cleanup) >= self.c:
+                            response_cleanup = response_cleanup[-self.c:]
+
+                        print(response_cleanup)
+                        
+                        # Check validity
+                        if (len(response_cleanup) == self.c
+                            and all(len(response_cleanup[i]) == pred_len for i in range(self.c))
+                            and all(all(len(response_cleanup[i][j]) == 2 for j in range(pred_len)) for i in range(5))):
+                            # Add the response to the dump list
+                            llm_processed_list[ped_idx].extend(response_cleanup)
+                            llm_response_list[ped_idx][prompt_idx] = response
+                            messages.append({"role": "assistant", "content": response})
+                            break
+                        else:
+                            timeout += 1
+                            error_code = 'Wrong response format'
+                            continue
+
+                    if timeout >= max_timeout:
+                        print("Chatbot Timeout! Error scene_idx: {} ped_idx: {} prompt_idx: {}".format(scene_idx, ped_idx, prompt_idx))
+                        break
+
+
         ground_truth = torch.cat([inputs['center_gt_trajs'][..., :2], inputs['center_gt_trajs_mask'].unsqueeze(-1)],
                                  dim=-1)
+
         
         output = {}
         output['predicted_probability'] = torch.rand(ground_truth.shape[0], self.c)  # [B, c]
